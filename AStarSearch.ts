0b8511fc7cada9c4c13a440f494221c1e1813c0c
Christopher <christophermeszaros07@gmail.com>
2018-01-26 08:59:17 +0100

    -Merged the the initial (start node) and the main search. Now it can compile without the complaining about the type
    -Refractored the compare function
    
    -Added potentially useful dictionaries.
    
    == Summary, out of 16 tests:
    
    Manhattan:  4 failed,  Time: 41.527 s,  Nodes: 10697,  Heuristic calls: 19166239
    ConstZero:  15 failed,  Time: 150.119 s,  Nodes: 1043,  Heuristic calls: 0
    
    Manhattan is 3.6x faster than ConstZero
    Manhattan creates 10.3x MORE nodes than ConstZero
    Manhattan heuristic is called 1791.7x more than the number of nodes created.
    
    ==>  PROBLEM: A* does not find the optimal path in all cases
    ==>  PROBLEM: Manhattan creates 10.3x more nodes than ConstZero
    ==>  PROBLEM: The Manhattan heuristic function is called 1791.7x more than the number of nodes created

--- AStarSearch.ts
+++ AStarSearch.ts
@@ -1,32 +1,17 @@
+/********************************************************************************
+This module contains A* search implementation, parameterised by a 'Node' type.
+@param graph: The graph on which to perform A* search.
+@param start: The initial node.
+@param goal: Function that returns true if the algorithm has reached the goal.
+@param heuristics: Function that estimates the heuristics cost of reaching the goal from a given Node.
+@param timeout: Maximum time (in seconds) to spend performing A* search.
+@returns: Returns a search result [path from start to goal, total cost, and some statistics].
+********************************************************************************/
 
 import {Successor, Graph, SearchResult} from "./Graph";
-
-// You might want to use one of these:
 import Set from "./lib/typescript-collections/src/lib/Set";
 import Dictionary from "./lib/typescript-collections/src/lib/Dictionary";
 import PriorityQueue from "./lib/typescript-collections/src/lib/PriorityQueue";
-
-/********************************************************************************
-** AStarSearch
-
-This module contains an implementation of the A* algorithm.
-You should change the function 'aStarSearch'. 
-********************************************************************************/
-
-/* A* search implementation, parameterised by a 'Node' type. 
- * The code here is just a template; you should rewrite this function entirely.
- * This template produces a dummy search result which is a random walk.
- *
- * Note that you should not change the API (type) of this function, only its body.
- *
- * @param graph: The graph on which to perform A* search.
- * @param start: The initial node.
- * @param goal: A function that returns true when given a goal node. Used to determine if the algorithm has reached the goal.
- * @param heuristics: The heuristic function. Used to estimate the cost of reaching the goal from a given Node.
- * @param timeout: Maximum time (in seconds) to spend performing A* search.
- * @returns: A search result, which contains the path from 'start' to a node satisfying 'goal', 
- *           the cost of this path, and some statistics.
- */
 
 export function aStarSearch<Node> (
     graph : Graph<Node>,
@@ -38,53 +23,55 @@ export function aStarSearch<Node> (
 
     // Define a class to represent a search node
     class SearchNode {
-        constructor(
-            public parentNode : SearchNode | undefined, // undefined if parent is start node
-            public nextEdge   : Successor<Node>,        // edge incident to the parent
-            public totalcost  : number,                 // total cost from start node
-            public astarcost  : number,                 // total cost plus heuristics cost
+        constructor( 
+            // undefined values are used only for the first search node (i.e. the start).
+            public parent    : SearchNode      | undefined, // parent search node
+            public edge      : Successor<Node> | undefined, // edge.child is the graph node
+            public totalcost : number,                      // total cost from start node
+            public astarcost : number,                      // total cost plus heuristics cost
         ) {};
     }
-    // Define a compare function
-    var compare : (a: SearchNode, b: SearchNode) => number;
-    compare = function(a: SearchNode, b: SearchNode) : number {
+    // Function to compare 2 searchnodes (a < b if a has higher cost than b).
+    function compare(a : SearchNode, b : SearchNode) : number {
         if(a.astarcost > b.astarcost) return -1;
         if(a.astarcost < b.astarcost) return 1;
         return 0;
     }
-    // Define function to compute the path
+    // Function to compute the path (acts as the backtracking step).
     function path(endNode : SearchNode) : Successor<Node>[] {
-        var p : Successor<Node>[] = [];
-        var curNode : SearchNode = endNode;
-        while(curNode.parentNode) {
-            p.push(curNode.nextEdge);
-            curNode = curNode.parentNode;
+        var path : Successor<Node>[] = [];
+        var node : SearchNode = endNode;
+        while(node.edge && node.parent) {
+            path.push(node.edge);
+            node = node.parent;
         }
-        return p.reverse();
+        return path.reverse();
     }
-    // Start the timer, define the frontier and the visited set
+    // Function to compute the heuristics cost for a specified node.
+    function heurcost(node : Node) : number {
+        var cost : number | undefined = hCostDict.getValue(node);
+        if(!cost) // if heuristic cost hasn't never been computed before
+            hCostDict.setValue(node, (cost = heuristics(node)));
+        return cost;
+    }
+    // Define some useful datastructures.
+    var visited   : Set<Node> = new Set();                               // set of visited nodes
+    var tCostDict : Dictionary<Node,number> = new Dictionary();             // total costs
+    var hCostDict : Dictionary<Node,number> = new Dictionary();             // heuristic costs
+    var frontier  : PriorityQueue<SearchNode> = new PriorityQueue(compare); // frontier
+
+    // Start the timer and initialize with the start node.
     var endTime = Date.now() + timeout * 1000;
-    var visited : Set<Node> = new Set();
-    var frontier : PriorityQueue<SearchNode> = new PriorityQueue<SearchNode>(compare);
-    
-    // Initialize the search with the start node
+    frontier.add(new SearchNode(undefined, undefined, 0, 0));
     visited.add(start);
-    if(goal(start))
-        return new SearchResult<Node>('success', [], 0, visited.size());
-    var successors : Successor<Node>[] = graph.successors(start);
-    for(var edge of successors) {
-        if(Date.now() >= endTime) break;
-        visited.add(edge.child);
-        var astarcost : number = edge.cost + heuristics(edge.child);
-        frontier.enqueue(new SearchNode(undefined, edge, edge.cost, astarcost));
-    }
-    // Searching begins here
+
+    // Searching begins here.    
     while(Date.now() < endTime) {
         var searchNode : SearchNode | undefined = frontier.dequeue();
         if(!searchNode) {
             return new SearchResult<Node>('failure', [], -1, visited.size());
         }
-        var graphNode : Node = searchNode.nextEdge.child;
+        var graphNode : Node = (searchNode.edge)? searchNode.edge.child : start;   //if the search node has an edge, then the graph node is equal to the child of that edge, otherwise the graph node is the start node. Test 
         if(goal(graphNode)) {
             return new SearchResult<Node>('success', path(searchNode), searchNode.totalcost, visited.size());
         }
@@ -94,7 +81,9 @@ export function aStarSearch<Node> (
             var totalcost : number = searchNode.totalcost + edge.cost;
             var astarcost : number = totalcost + heuristics(edge.child);
             frontier.enqueue(new SearchNode(searchNode, edge, totalcost, astarcost));
+            }
         }
+	return new SearchResult<Node>('timeout', [], -1, visited.size());
     }
-    return new SearchResult<Node>('timeout', [], -1, visited.size());
-}
+  
+
