e4247add73da9712bcde532a8628461b3ac84ea1
Renjith <renjithseban@gmail.com>
2018-01-27 07:04:52 +0100

    Modified the compare function. Test Status: Manhattan (15 passed, 1 failed)              CountZero (1 passed, 15 failed) DON'T MAKE test-astar.js. Run by compiling the test-astar.ts ($ tsc test-astar.ts, $node test-astar.js 'all')

--- AStarSearch.ts
+++ AStarSearch.ts
@@ -38,69 +38,76 @@ export function aStarSearch<Node> (
 
     // Define a class to represent a search node
     class SearchNode {
-        public parentNode: SearchNode | undefined;
-        public graphNode: Successor<Node> | undefined;
-        public totalCost: number;
-        public astarcost: number;
-        constructor(parent: SearchNode, node: Successor<Node>, cost: number) {
+        public parentNode: SearchNode | undefined;		// * parentNode	: keeps the parent SearchNode of current node under search *
+        public graphNode: Successor<Node> | undefined;	// * graphNode 	: keeps the corresponding graphNode of current node under search *
+        public totalCost: number;						// * totalCost	: total cost to reach the current node under search	from start node *
+        public astarcost: number;						// * astarcost  : sum of totalCost and heuristic value of current node under search *
+
+        // * Constructor to computes the astarcost before adding a node to PriorityQueue *
+        constructor(parent: SearchNode, node: Successor<Node> | undefined, cost: number) {
+        	var curNode: Node = (node) ? node.child : start;	// ** Successor<Node> is undefined for start node **
             this.parentNode = parent;
             this.graphNode = node;
             this.totalCost = cost;
-            this.astarcost = (this.graphNode) ? this.totalCost + heuristics(this.graphNode.child) : heuristics(start);
+            this.astarcost = this.totalCost + heuristics(curNode);
         }
     }
-    // Define a compare function
+
+    // * Define a compare function for PriorityQueue *
     var compare : (a: SearchNode, b: SearchNode) => number;
     compare = function(a: SearchNode, b: SearchNode) : number {
         if(a.astarcost > b.astarcost) return -1;
         if(a.astarcost < b.astarcost) return 1;
+
+        // * if astarcost are same, one with minimum heuristic value is kept on top of queue *
+
+        if((a.astarcost - a.totalCost) < (b.astarcost - b.totalCost)) return 1;	
+        if((a.astarcost - a.totalCost) > (b.astarcost - b.totalCost)) return -1;
         return 0;
     }
-    // Define function to compute the path
 
+    // * Define function to compute the path via backtracking *
     function path(endNode : Node) : Successor<Node>[] {
         var path : Successor<Node>[] = [];
-        var node : Node = endNode;
-        var curSearchNode: SearchNode | undefined = nodeDictionary.getValue(node);
+        var curSearchNode: SearchNode | undefined = nodeDictionary.getValue(endNode); // * Dictionary keeps the minimum cost path to a node from start node *
         while(curSearchNode) {
             path.push(curSearchNode.graphNode);
             curSearchNode = (curSearchNode.parentNode.graphNode) ? nodeDictionary.getValue(curSearchNode.parentNode.graphNode.child) : undefined;
         }
         return path.reverse();
     }
-    // Start the timer, define the frontier and the visited set
-    var currentSearchNode: SearchNode | undefined;
-    var currentNode: Node;
-    var nodeDictionary: Dictionary<Node, SearchNode|undefined> = new Dictionary<Node,SearchNode|undefined>();
+
+    // Variable declaration
+    var currentSearchNode: SearchNode | undefined;	// * to keep the SearchNode dequeued from the PriorityQueue
+    var currentNode: Node;							// * to keep the GraphNode to currentSearchNode
+    var nodeDictionary: Dictionary<Node, SearchNode | undefined> = new Dictionary<Node,SearchNode | undefined>();	// * to keep track of visisted nodes
     var endTime = Date.now() + timeout * 1000;
-    var frontier : PriorityQueue<SearchNode> = new PriorityQueue<SearchNode>(compare);
+    var frontier : PriorityQueue<SearchNode> = new PriorityQueue<SearchNode>(compare); 
     var successors: Successor<Node>[] = [];
 
-    frontier.enqueue(new SearchNode(undefined, undefined,0));
+    frontier.enqueue(new SearchNode(undefined, undefined,0)); // * specific to start node *
     
     // Searching begins here
     while(Date.now() < endTime) {
         currentSearchNode = frontier.dequeue();
-
         if(!currentSearchNode) {
             return new SearchResult<Node>('failure', [], -1, nodeDictionary.keys.length + frontier.size());
         }
-
         if(currentSearchNode.graphNode){
             currentNode = currentSearchNode.graphNode.child;
             if(!nodeDictionary.containsKey(currentNode)){
-                nodeDictionary.setValue(currentNode, currentSearchNode);
+                nodeDictionary.setValue(currentNode, currentSearchNode); // * Add to dictionary if it's not already visisted *
             }
             else{
                 var val:SearchNode = nodeDictionary.getValue(currentNode);
                 if(val.astarcost > currentSearchNode.astarcost){
-                    nodeDictionary.setValue(currentNode, currentSearchNode);
+                    nodeDictionary.setValue(currentNode, currentSearchNode); // * Update the value in dictionary if found another minimum cost path *
                 }
             }
         }
         else{
             currentNode = start;
-            nodeDictionary.setValue(start, undefined);
+            nodeDictionary.setValue(start, undefined); // * Initialize the dictionary with start node *
         }
 
         if(goal(currentNode)) {
