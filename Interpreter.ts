18031ea287930282752bbfec3609f7c34ab137c2
Renjith Sebastian <renjithseban@gmail.com>
2018-02-04 08:36:00 +0000

    Changes related to interpretObject() method

--- Interpreter.ts
+++ Interpreter.ts
@@ -1,5 +1,5 @@
 
-import {WorldState} from "./World";
+import { WorldState } from "./World";
 
 import {
     ShrdliteResult,
@@ -8,6 +8,8 @@ import {
     Object, RelativeObject, SimpleObject,
     DNFFormula, Conjunction, Literal,
 } from "./Types";
+
+import Dictionary from "./lib/typescript-collections/src/lib/Dictionary";
 
 /********************************************************************************
 ** Interpreter
@@ -49,14 +51,14 @@ You should implement the function 'interpretCommand'.
  *           If there's an interpretation error, it throws an error with a string description.
  */
 
-export function interpret(parses : ShrdliteResult[], world : WorldState) : ShrdliteResult[] {
-    var errors : string[] = [];
-    var interpretations : ShrdliteResult[] = [];
-    var interpreter : Interpreter = new Interpreter(world);
+export function interpret(parses: ShrdliteResult[], world: WorldState): ShrdliteResult[] {
+    var errors: string[] = [];
+    var interpretations: ShrdliteResult[] = [];
+    var interpreter: Interpreter = new Interpreter(world);
     for (var result of parses) {
         try {
-            var intp : DNFFormula = interpreter.interpretCommand(result.parse);
-        } catch(err) {
+            var intp: DNFFormula = interpreter.interpretCommand(result.parse);
+        } catch (err) {
             errors.push(err);
             continue;
         }
@@ -80,8 +82,9 @@ export function interpret(parses : ShrdliteResult[], world : WorldState) : Shrdl
 
 class Interpreter {
     constructor(
-        private world : WorldState
-    ) {}
+        private world: WorldState
+
+    ) { }
 
     /* The main interpretation method.
      * Note that you should not change the API (type) of this method, only its body.
@@ -93,25 +96,26 @@ class Interpreter {
      *           If there's an interpretation error, it throws an error with a string description.
      */
 
-    public interpretCommand(cmd : Command) : CommandSemantics {
+    public interpretCommand(cmd: Command): CommandSemantics {
         // This currently returns a dummy interpretation involving one or two random objects in the world.
         // Instead it should call the other interpretation methods for
         // each of its arguments (cmd.entity and/or cmd.location).
-        var interpretation : CommandSemantics;
-
-        var all_objects : string[] = Array.prototype.concat.apply([], this.world.stacks);
+        var interpretation: CommandSemantics;
+        // this.interpretEntity(cmd)
+        var all_objects: string[] = Array.prototype.concat.apply([], this.world.stacks);
         if (this.world.holding) {
             all_objects.push(this.world.holding);
         }
-
         if (cmd instanceof MoveCommand) {
+            this.interpretEntity(cmd.entity);
             var a = all_objects[Math.floor(Math.random() * all_objects.length)];
             var b = all_objects[Math.floor(Math.random() * all_objects.length)];
             if (a == b) {
                 throw "Cannot put an object ontop of itself";
             }
+
             interpretation = new DNFFormula([
-                new Conjunction([ // ontop(a, b) & ontop(b, floor)
+                new Conjunction([
                     new Literal("ontop", [a, b]),
                     new Literal("ontop", [b, "floor"])
                 ])
@@ -146,22 +150,140 @@ class Interpreter {
         else {
             throw "Unknown command";
         }
-
         return interpretation;
     }
 
-    interpretEntity(ent : Entity) : EntitySemantics {
+
+
+    interpretEntity(ent: Entity): EntitySemantics {
+        var obj: ObjectSemantics = this.interpretObject(ent.object);
         throw "Not implemented";
     }
 
-    interpretLocation(loc : Location) : LocationSemantics {
+    interpretLocation(loc: Location): LocationSemantics {
         throw "Not implemented";
     }
 
-    interpretObject(obj : Object) : ObjectSemantics {
-        throw "Not implemented";
-    }
 
+
+    interpretObject(obj: Object): ObjectSemantics {
+        try {
+            let result: ObjectSemantics = [];
+            let items: { [s: string]: SimpleObject } = this.world.objects;
+            //function to compare size of two SimpleObject
+            let size = function (x: SimpleObject, y: SimpleObject): boolean {
+                return ((x.size && x.size == y.size) || (!x.size));
+            }
+            //function to compare color of two SimpleObject
+            let color = function (x: SimpleObject, y: SimpleObject): boolean {
+                return ((x.color && x.color == y.color) || (!x.color));
+            }
+            if (obj instanceof SimpleObject) {
+                let simpleObj: SimpleObject;
+                for (var key in items) {
+                    simpleObj = items[key];
+                    if (obj.form == "anyform" && size(obj, simpleObj) && color(obj, simpleObj)) {
+                        result.push(key);
+                    }
+                    else {
+                        if (obj.form == simpleObj.form && size(obj, simpleObj) && color(obj, simpleObj)) {
+                            result.push(key);
+                        }
+                    }
+                }
+            }
+            else if (obj instanceof RelativeObject) {
+
+                //variable declaration
+                let stackPositions: Dictionary<string, number> = new Dictionary(); //to keep stack position of an object
+                let indexPositions: Dictionary<string, number> = new Dictionary(); //to keep the index position of an object in a stack
+                let objects: ObjectSemantics = this.interpretObject(obj.object);
+                let location: LocationSemantics = this.interpretLocation(obj.location);
+                let stackPosKey: number | undefined;
+                let stackPosLocation: number | undefined;
+                let indexPosKey: number | undefined;
+                let indexPosLocation: number | undefined;
+
+                //loading the dictionaries based on the current world state
+                this.world.stacks.forEach(function (items, stackPos) {
+                    items.forEach(function (item, indexPos) {
+                        stackPositions.setValue(item, stackPos);
+                        indexPositions.setValue(item, indexPos);
+                    });
+                });
+
+                objects.forEach(function (key) {
+                    stackPosKey = stackPositions.getValue(key);
+                    indexPosKey = indexPositions.getValue(key);
+                    let isValid: boolean = true;
+                    location.entity.object.forEach(function (locationKey) {
+                        stackPosLocation = stackPositions.getValue(locationKey);
+                        indexPosLocation = indexPositions.getValue(locationKey);
+                        if (stackPosKey && stackPosLocation && indexPosKey && indexPosLocation) {
+                            switch (location.relation) {
+                                case "ontopof":
+                                case "inside":
+                                    if (stackPosKey == stackPosLocation && indexPosLocation + 1 == indexPosKey) {
+                                        if ((location.entity.quantifier == "any" || location.entity.quantifier == "the") && result.indexOf(key) != -1)
+                                            result.push(key);
+                                        else if (location.entity.quantifier == "all")
+                                            throw 'Things can only be $(location.relation) exactly one object';
+                                    }
+                                    break;
+                                case "leftof":
+                                    if (stackPosKey < stackPosLocation) {
+                                        if ((location.entity.quantifier == "any" || location.entity.quantifier == "the") && result.indexOf(key) != -1)
+                                            result.push(key);
+                                    }
+                                    else
+                                        isValid = false;
+                                    break;
+                                case "rightof":
+                                    if (stackPosKey > stackPosLocation) {
+                                        if ((location.entity.quantifier == "any" || location.entity.quantifier == "the") && result.indexOf(key) != -1)
+                                            result.push(key);
+                                    }
+                                    else
+                                        isValid = false;
+                                    break;
+                                case "under":
+                                    if (stackPosKey == stackPosLocation && indexPosKey < indexPosLocation) {
+                                        if ((location.entity.quantifier == "any" || location.entity.quantifier == "the") && result.indexOf(key) != -1)
+                                            result.push(key);
+                                    }
+                                    else
+                                        isValid = false;
+                                    break;
+                                case "above":
+                                    if (stackPosKey == stackPosLocation && indexPosKey > indexPosLocation && result.indexOf(key) == -1) {
+                                        if ((location.entity.quantifier == "any" || location.entity.quantifier == "the") && result.indexOf(key) != -1)
+                                            result.push(key);
+                                    }
+                                    else
+                                        isValid = false;
+                                    break;
+                                case "beside":
+                                    if (stackPosKey == stackPosLocation - 1 || stackPosKey == stackPosLocation + 1) {
+                                        if ((location.entity.quantifier == "any" || location.entity.quantifier == "the") && result.indexOf(key) != -1)
+                                            result.push(key);
+                                    }
+                                    else
+                                        isValid = false;
+                            }
+                        }
+                    });
+                    if (location.entity.quantifier == "all" && isValid)
+                        result.push(key);
+                });
+            }
+            if (result.length == 0)
+                throw "Couldn't find any matching object";
+            return result;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
 }
 
 
@@ -170,14 +292,14 @@ class Interpreter {
 // of the different parse result classes.
 
 // This is the main interpretation result, a DNF formula
-type CommandSemantics  = DNFFormula
+type CommandSemantics = DNFFormula
 
 // The semantics of an object description is a collection of
 // the objects that match the description
-type ObjectSemantics   = string[]
+type ObjectSemantics = string[]
 
 // The semantics of an Entity or a Location is just a wrapper
 // around the semantics of its children
-type EntitySemantics   = {quantifier : string; object : ObjectSemantics}
-type LocationSemantics = {relation : string; entity : EntitySemantics}
+type EntitySemantics = { quantifier: string; object: ObjectSemantics }
+type LocationSemantics = { relation: string; entity: EntitySemantics }
 
