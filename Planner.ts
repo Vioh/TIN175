f55479bcf98ac09b96690c630fd5c2e20dcdc43d
Hai Dinh <hasy@student.chalmers.se>
2018-02-24 10:27:15 +0100

    Added the heuristics functions. Very bad codes though. Needs lots of refactoring

--- Planner.ts
+++ Planner.ts
@@ -68,10 +68,8 @@ class ShrdliteGraph implements Graph<ShrdliteNode> {
 class ShrdliteNode {
     public id : string;
     public state : WorldState;
-    private readonly objects : {[s:string]: SimpleObject};
 
-    constructor(state : WorldState, objects : {[s:string]: SimpleObject}) {
-        this.state = state;
+    constructor(state : WorldState) {
         this.id = `${state.arm},${state.holding},${this.stringify(state.stacks)}`;
     }
     public toString() : string {
@@ -88,22 +86,22 @@ class ShrdliteNode {
 
         if(action == 'l') {
             if(xpos == 0) return null;
-            let newState = this.clone(this.state);
+            let newState : WorldState = this.clone(this.state);
             --newState.arm; // go left one step
-            return new ShrdliteNode(newState, this.state.objects);
+            return new ShrdliteNode(newState);
         }
         if(action == 'r') {
             if(xpos == stacks.length-1) return null;
-            let newState = this.clone(this.state);
+            let newState : WorldState = this.clone(this.state);
             ++newState.arm; // go right one step
-            return new ShrdliteNode(newState, this.state.objects);
+            return new ShrdliteNode(newState);
         }
         if(action == 'p') {
             if(holding || ypos == -1) return null;
-            let newState = this.clone(this.state);
+            let newState : WorldState = this.clone(this.state);
             newState.holding = newState.stacks[xpos][ypos];
             newState.stacks[xpos].splice(ypos, 1);
-            return new ShrdliteNode(newState, this.state.objects);
+            return new ShrdliteNode(newState);
         }
         if(action == 'd') {
             let dest = (ypos == -1)? "floor" : stacks[xpos][ypos];
@@ -112,7 +110,7 @@ class ShrdliteNode {
             if(!newState.holding) return null; // dummy check to pass compiler's type checker
             newState.stacks[xpos].push(newState.holding);
             newState.holding = null;
-            return new ShrdliteNode(newState, this.state.objects);
+            return new ShrdliteNode(newState);
         }
         return null;
     }
@@ -127,14 +125,14 @@ class ShrdliteNode {
             "stacks": JSON.parse(JSON.stringify(state.stacks)),
             "holding": state.holding,
             "arm": state.arm,
+            "objects": state.objects,
             "examples": [], // examples are not needed for the planer
-            "objects": {},  // object definitions are not needed for the planer
         };
     }
     private isValidDrop(obj1 : string, obj2 : string) : boolean {
         let floor : SimpleObject = new SimpleObject("floor", null, null);
-        let a : SimpleObject = this.objects[obj1];
-        let b : SimpleObject = (obj2 == "floor")? floor : this.objects[obj2];
+        let a : SimpleObject = (obj1 == "floor")? floor : this.state.objects[obj1];
+        let b : SimpleObject = (obj2 == "floor")? floor : this.state.objects[obj2];
 
         function memberOf(needle : string, haystack : string[]) : boolean {
             return haystack.indexOf(needle) > -1;
@@ -173,9 +171,31 @@ function coordinate(obj : string, state : WorldState) : {x : number, y : number}
     return {"x" : -1, "y" : -1}; // this is for the floor
 }
 
+/** Returns true if the literal (binary relation) is true in the world state. */
+function checkBinaryRelation(lit : Literal, state : WorldState) : boolean {
+    let rel = lit.relation;
+    let coorA = coordinate(lit.args[0], state); // coordinate of object A
+    let coorB = coordinate(lit.args[1], state); // coordinate of object B
+    
+    // Case 1: A and B are on the same stack of objects.
+    if(lit.args[1] == "floor" || coorA.x == coorB.x) {
+        if(rel == "ontop"  && coorA.y == coorB.y + 1) return true;
+        if(rel == "inside" && coorA.y == coorB.y + 1) return true;
+        if(rel == "above"  && coorA.y > coorB.y) return true;
+        if(rel == "under"  && coorA.y < coorB.y) return true;
+    } 
+    // Case 2: a and b are on 2 different stacks of objects
+    else {
+        if(rel == "beside"  && Math.abs(coorA.x - coorB.x) == 1) return true;
+        if(rel == "leftof"  && coorA.x < coorB.x) return true;
+        if(rel == "rightof" && coorA.x > coorB.x) return true;
+    }
+    return false;
+}
+
 /** Returns a specialized goal test for a ShrdliteNode */
 function goalTest(interpretation : DNFFormula) : (node : ShrdliteNode) => boolean {
-    return function(node : ShrdliteNode) {
+    return function(node : ShrdliteNode) : boolean {
         for(let conj of interpretation.conjuncts)
             if(isTrueConj(conj, node.state)) return true;
         return false;
@@ -194,25 +214,41 @@ function goalTest(interpretation : DNFFormula) : (node : ShrdliteNode) => boolea
         }
         return false;
     }
-    function checkBinaryRelation(lit : Literal, state : WorldState) : boolean {
-        let rel = lit.relation;
-        let coorA = coordinate(lit.args[0], state); // coordinate of object A
-        let coorB = coordinate(lit.args[1], state); // coordinate of object B
-        
-        // Case 1: A and B are on the same stack of objects.
-        if(lit.args[1] == "floor" || coorA.x == coorB.x) {
-            if(rel == "ontop"  && coorA.y == coorB.y + 1) return true;
-            if(rel == "inside" && coorA.y == coorB.y + 1) return true;
-            if(rel == "above"  && coorA.y > coorB.y) return true;
-            if(rel == "under"  && coorA.y < coorB.y) return true;
-        } 
-        // Case 2: a and b are on 2 different stacks of objects
-        else {
-            if(rel == "beside"  && Math.abs(coorA.x - coorB.x) == 1) return true;
-            if(rel == "leftof"  && coorA.x < coorB.x) return true;
-            if(rel == "rightof" && coorA.x > coorB.x) return true;
+}
+
+// ===============================================================================================
+// ===============================================================================================
+// ===============================================================================================
+
+/** Returns a specialized function to compute the heuristics for a ShrdliteNode */
+function heuristics(intp : DNFFormula) : (node : ShrdliteNode) => number {
+    return function(node : ShrdliteNode) : number {
+        let heurs : number[] = intp.conjuncts.map((conj) => heurForConj(conj, node.state));
+        return Math.min(...heurs);
+    }
+    function heurForConj(conj : Conjunction, state : WorldState) : number {
+        let heurs : number[] = conj.literals.map((lit) => heurForLit(lit, state));
+        return Math.max(...heurs);
+    }
+    function heurForLit(lit : Literal, state : WorldState) : number {
+        let numOfOper = 4;
+        if(lit.relation == "holding") {
+            if(state.holding == lit.args[0]) return 0;
+            let coor = coordinate(lit.args[0], state);
+            let numOntop = state.stacks[coor.x].length - coor.y - 1;
+            return numOfOper * numOntop;
         }
-        return false;
+        if(checkBinaryRelation(lit, state)) return 0; // goal state
+        let coorA = coordinate(lit.args[0], state);
+        let coorB = coordinate(lit.args[1], state);
+        let numOntopA = state.stacks[coorA.x].length - coorA.y - 1;
+        let numOntopB = state.stacks[coorA.x].length - coorA.y - 1;
+        if(coorA.x != coorB.x) {
+            if(lit.args[1] == "floor") return numOfOper * numOntopA;
+            return numOfOper * (numOntopA + numOntopB);
+        } else {
+            return numOfOper * Math.min(numOntopA, numOntopB);
+        }
     }
 }
 
@@ -221,67 +257,24 @@ function goalTest(interpretation : DNFFormula) : (node : ShrdliteNode) => boolea
 // ===============================================================================================
 
 class Planner {
-    constructor(
-        private world : WorldState
-    ) {}
-
+    constructor(private world : WorldState) {}
     /** 
      * The core planner method.
      * @param interpretation: The logical interpretation of the user's desired goal. 
      * @returns: A plan, represented by a list of strings.
      *           If there's a planning error, it throws an error with a string description.
      */
-    makePlan(interpretation : DNFFormula) : string[] {
-        var state = this.world;
-        var plan : string[] = [];
-
-        // Select a random nonempty stack
-        do {
-            var pickstack = Math.floor(Math.random() * state.stacks.length);
-        } while (state.stacks[pickstack].length == 0);
-
-        // First move the arm to the selected stack
-        if (pickstack < state.arm) {
-            plan.push("Moving left to stack " + pickstack);
-            for (var i = state.arm; i > pickstack; i--) {
-                plan.push("l");
-            }
-        } else if (pickstack > state.arm) {
-            plan.push("Moving right to stack " + pickstack);
-            for (var i = state.arm; i < pickstack; i++) {
-                plan.push("r");
-            }
-        }
-        // Then pick up the topmost object in the selected stack
-        var obj = state.stacks[pickstack][state.stacks[pickstack].length-1];
-        plan.push("Picking up the " + state.objects[obj].form,
-                  "p");
-
-        if (pickstack > 0) {
-            // Then move the arm to the leftmost stack
-            plan.push("Moving as far left as possible");
-            for (var i = pickstack; i > 0; i--) {
-                plan.push("l");
-            }
-        }
-        // Select a random destination stack (either empty or the original pickup stack)
-        do {
-            var dropstack = Math.floor(Math.random() * state.stacks.length);
-        } while (!(state.stacks[dropstack].length == 0 || dropstack == pickstack));
-
-        if (dropstack > 0) {
-            // Then move the arm to the destination stack
-            plan.push("Moving right to the destination stack " + dropstack);
-            for (var i = 0; i < dropstack; i++) {
-                plan.push("r");
-            }
-        }
-        // Finally put the object down again
-        plan.push("Dropping the " + state.objects[obj].form,
-                  "d");
-
-        return plan;
+    makePlan(intp : DNFFormula) : string[] {
+        let start : ShrdliteNode = new ShrdliteNode(this.world);
+        let result = aStarSearch(new ShrdliteGraph(), start, goalTest(intp), heuristics(intp), 10);
+        if(result.status == "timeout")
+            throw `TIMEOUT! Visited ${result.visited} nodes`;
+        if(result.status == "failure")
+            throw `No path exists from start to goal`;
+        return result.path.map((incomingEdge) => incomingEdge.action);
     }
 }
 
 // TODO: Throw errors in plannner at appropriae places!
+// TODO: Test timeout "medium" "put the brick that is to the left of a pyramid in a box"
+// leftof, rightof, inside, ontop, under, beside, above, HOLDING
